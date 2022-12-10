78b1bd73cff343abab415365abeeccab402e703e
CaoY <3145213279@qq.com>
2022-12-11 01:20:09 +0800

    第一次建立示例工程

---
+++ src/main/java/com/mine/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.mine.demo;
+
+/**
+ * @author CaoY
+ * @date 2022-12-11 1:12
+ * @description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, SpringBootWorld!");
+        System.out.println("This is git test!");
+    }
+}
